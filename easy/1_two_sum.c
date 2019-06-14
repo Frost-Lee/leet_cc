080e2e6fc5319da5b649f31c39b463fcb691655b
Frost Lee <canchen.lee@gmail.com>
2019-06-14 17:20:15 +0200

    two sum updated.

---
+++ easy/1_two_sum.c
@@ -0,0 +1,18 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    for (int i = 0; i < numsSize; i ++) {
+        for (int j = i + 1; j < numsSize; j ++) {
+            if (nums[i] + nums[j] == target) {
+                int* rst = (int*)malloc(2 * sizeof(int));
+                rst[0] = i;
+                rst[1] = j;
+                *returnSize = 2;
+                return rst;
+            }
+        }
+    }
+    *returnSize = 0;
+    return NULL;
+}
