c36c2842f3057149fb42287af87d3b9e90f3bc5b
Frost Lee <canchen.lee@gmail.com>
2020-03-01 16:53:17 -0500

    search matrix 2.

--- medium/240_search_a_2d_matrix_ii.c
+++ medium/240_search_a_2d_matrix_ii.c
@@ -1,6 +1,5 @@
 bool searchMatrix(int** matrix, int matrixRowSize, int matrixColSize, int target) {
     int row = matrixRowSize - 1, col = 0;
-    if (matrixRowSize == 0 || matrixColSize == 0) {return false;}
     while (row >= 0 && col < matrixColSize) {
         int value = matrix[row][col];
         if (value == target) {
