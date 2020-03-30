470b5c778137adf82edfe65aeda359800c68b928
Frost Lee <canchen.lee@gmail.com>
2020-03-30 19:05:52 -0400

    rotate image.

--- medium/48_rotate_image.c
+++ medium/48_rotate_image.c
@@ -1,8 +1,8 @@
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
     int rowEnd = matrixSize / 2;
     int colEnd = (matrixSize + 1) / 2;
-    for (int row = 0; row < matrixSize / 2; row ++) {
-        for (int col = 0; col < (matrixSize + 1) / 2; col ++) {
+    for (int row = 0; row < rowEnd; row ++) {
+        for (int col = 0; col < colEnd; col ++) {
             int temp = matrix[row][col];
             int oppositeCol = matrixSize - col - 1;
             int oppositeRow = matrixSize - row - 1;
