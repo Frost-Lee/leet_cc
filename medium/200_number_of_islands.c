592fd36d92547ea1b3a1a7a8a64e03736504ac6c
Frost Lee <canchen.lee@gmail.com>
2020-04-01 21:09:42 -0400

    number of islands update.

--- medium/200_number_of_islands.c
+++ medium/200_number_of_islands.c
@@ -9,29 +9,15 @@ void depthFirstSearch(char** grid, int sourceRow, int sourceCol, int rowSize, in
     depthFirstSearch(grid, sourceRow, sourceCol + 1, rowSize, colSize);
 }
 
-void firstLandIndex(char **grid, int rowSize, int colSize, int* row, int* col) {
-    for (int i = 0; i < rowSize; i ++) {
-        for (int j = 0; j < colSize; j ++) {
-            if (grid[i][j] != '0') {
-                *row = i;
-                *col = j;
-                return;
+int numIslands(char** grid, int gridSize, int* gridColSize){
+    int islandCount = 0;
+    for (int i = 0; i < gridSize; i ++) {
+        for (int j = 0; j < *gridColSize; j ++) {
+            if (grid[i][j] == '1') {
+                depthFirstSearch(grid, i, j, gridSize, *gridColSize);
+                islandCount ++;
             }
         }
-    }
-    *row = -1;
-    *col = -1;
-}
-
-int numIslands(char** grid, int gridSize, int* gridColSize){
-    if (gridSize == 0 || * gridColSize == 0) {return 0;}
-    int islandCount = 0;
-    int sourceRowIndex = 0, sourceColIndex = 0;
-    firstLandIndex(grid, gridSize, *gridColSize, &sourceRowIndex, &sourceColIndex);
-    while (sourceRowIndex >= 0 && sourceColIndex >= 0) {
-        depthFirstSearch(grid, sourceRowIndex, sourceColIndex, gridSize, *gridColSize);
-        islandCount ++;
-        firstLandIndex(grid, gridSize, *gridColSize, &sourceRowIndex, &sourceColIndex);
     }
     return islandCount;
 }
