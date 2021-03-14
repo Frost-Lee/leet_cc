4905383221e71a519735fc684bde4dacd66dd375
Canchen Li <canchen.lee@gmail.com>
2021-03-14 16:13:44 +0800

    lc 3

--- medium/3_longest_substring_without_repeating_characters.c
+++ medium/3_longest_substring_without_repeating_characters.c
@@ -1,63 +1,13 @@
-/**
- * Definition for a list node.
- * struct ListNode {
- *     int val;
- *     struct ListNode* next;
- * };
- */
-
-struct ListNode* appendNode(struct ListNode* tail, int val) {
-    struct ListNode* newTail = (struct ListNode*)malloc(sizeof(struct ListNode));
-    newTail->val = val;
-    newTail->next = NULL;
-    if (tail != NULL) {
-        tail->next = newTail;
-    }
-    return newTail;
-}
-
-void freeList(struct ListNode* head) {
-    while (head != NULL) {
-        struct ListNode* next = head->next;
-        free(head);
-        head = next;
-    }
-}
-
-void reduce(struct ListNode** headPtr, int* hash, int* hashPosition, char conflict) {
-    char thisChar;
-    do {
-        thisChar = (*headPtr)->val;
-        hash[(*headPtr)->val] = 0;
-        hashPosition[(*headPtr)->val] = 0;
-        struct ListNode* oldHead = *headPtr;
-        *headPtr = (*headPtr)->next;
-        free(oldHead);
-    } while (thisChar != conflict);
-}
-
-
-int lengthOfLongestSubstring(char * s){
-    int currentPosition = 0, slen = strlen(s);
-    int hash[129] = {};
-    int hashPosition[129] = {};
-    int max = 0, localMax = 0;
-    struct ListNode* ucharCacheHead = appendNode(NULL, 128);
-    struct ListNode* ucharCacheTail = ucharCacheHead;
-    while (currentPosition + max - localMax < slen) {
-        ucharCacheTail = appendNode(ucharCacheTail, *(s + currentPosition));
-        if (hash[*(s + currentPosition)] == 0) {
-            localMax += 1;
-            if (localMax > max) {max = localMax;}
-        } else {
-            int conflictPosition = hashPosition[*(s + currentPosition)];
-            localMax = (currentPosition - conflictPosition);
-            reduce(&ucharCacheHead, hash, hashPosition, *(s + currentPosition));
+int lengthOfLongestSubstring(char * s) {
+    short hashmap[128] = {};
+    int head = 0, max_length = 0;
+    for (int i = 0; i < strlen(s); i ++) {
+        while (hashmap[s[i]] != 0) {
+            hashmap[s[head]] = 0;
+            head += 1;
         }
-        hash[*(s + currentPosition)] = 1;
-        hashPosition[*(s + currentPosition)] = currentPosition;
-        currentPosition += 1;
+        hashmap[s[i]] = 1;
+        max_length = fmax(max_length, i - head + 1);
     }
-    freeList(ucharCacheHead);
-    return max;
+    return max_length;
 }
