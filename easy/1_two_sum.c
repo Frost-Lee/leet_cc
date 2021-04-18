5d508930881d715e478a89b59429aae1b678fb57
Canchen Li <canchen.lee@gmail.com>
2021-04-18 19:03:32 +0800

    lc 1

--- easy/1_two_sum.c
+++ easy/1_two_sum.c
@@ -1,18 +1,32 @@
-/**
- * Note: The returned array must be malloced, assume caller calls free().
- */
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+int* globalNums = NULL;
+
+int compare( const void* a, const void* b) {
+     int int_a = globalNums[*(unsigned short*)a], int_b = globalNums[*(unsigned short*)b];
+     return int_a < int_b ? -1 : int_a > int_b ? 1 : 0;
+}
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    globalNums = nums;
+    int* result = (int*)malloc(sizeof(int) * 2);
+    unsigned short* indexList = (unsigned short*)malloc(sizeof(unsigned short) * numsSize);
     for (int i = 0; i < numsSize; i ++) {
-        for (int j = i + 1; j < numsSize; j ++) {
-            if (nums[i] + nums[j] == target) {
-                int* rst = (int*)malloc(2 * sizeof(int));
-                rst[0] = i;
-                rst[1] = j;
-                *returnSize = 2;
-                return rst;
-            }
+        indexList[i] = i;
+    }
+    qsort(indexList, numsSize, sizeof(unsigned short), compare);
+    int start = 0, end = numsSize - 1;
+    while (start < end) {
+        int sum = nums[indexList[start]] + nums[indexList[end]];
+        if (sum < target) {
+            start += 1;
+        } else if (sum > target) {
+            end -= 1;
+        } else {
+            result[0] = indexList[start];
+            result[1] = indexList[end];
+            break;
         }
     }
-    *returnSize = 0;
-    return NULL;
+    free(indexList);
+    *returnSize = 2;
+    return result;
 }
