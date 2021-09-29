6ff413507d0d18cafe3ebc0daa691e0df183eefe
Canchen Li <canchen.lee@gmail.com>
2021-09-29 12:57:17 -0400

    lc 201

---
+++ medium/201_bitwise_and_of_numbers_range.c
@@ -0,0 +1,9 @@
+int rangeBitwiseAnd(int left, int right){
+    int shift = 0;
+    while (left != right) {
+        left = left >> 1;
+        right = right >> 1;
+        shift += 1;
+    }
+    return left << shift;
+}
