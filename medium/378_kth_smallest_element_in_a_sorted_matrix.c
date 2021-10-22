6cbe840d040566c59cb68ffcdd5f7a8cb8d45c03
Canchen Li <canchen.lee@gmail.com>
2021-10-22 12:41:12 -0400

    lc 378

--- medium/378_kth_smallest_element_in_a_sorted_matrix.c
+++ medium/378_kth_smallest_element_in_a_sorted_matrix.c
@@ -1,19 +1,3 @@
-int countNoGreaterThan(int** matrix, int matrixSize, int target);
-    
-int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int k) {
-    int head = matrix[0][0], tail = matrix[matrixSize - 1][matrixSize - 1];
-    while (head < tail) {
-        int middle = (head + tail) / 2;
-        int noGreaterThanMiddleCount = countNoGreaterThan(matrix, matrixSize, middle);
-        if (noGreaterThanMiddleCount < k) {
-            head = middle + 1;
-        } else {
-            tail = middle;
-        }
-    }
-    return tail;
-}
-
 int countNoGreaterThan(int** matrix, int matrixSize, int target) {
     int row = matrixSize - 1, col = 0;
     int count = 0;
@@ -26,4 +10,18 @@ int countNoGreaterThan(int** matrix, int matrixSize, int target) {
         }
     }
     return count;
+}
+
+int kthSmallest(int** matrix, int matrixSize, int* matrixColSize, int k) {
+    int head = matrix[0][0], tail = matrix[matrixSize - 1][matrixSize - 1];
+    while (head <= tail) {
+        int middle = (head + tail) / 2;
+        int noGreaterThanMiddleCount = countNoGreaterThan(matrix, matrixSize, middle);
+        if (noGreaterThanMiddleCount < k) {
+            head = middle + 1;
+        } else {
+            tail = middle - 1;
+        }
+    }
+    return head;
 }
