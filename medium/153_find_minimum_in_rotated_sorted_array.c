fac7030ac0796f078da0024c93600e8385abc790
Canchen Li <canchen.lee@gmail.com>
2021-10-29 01:10:27 -0400

    lc 153

---
+++ medium/153_find_minimum_in_rotated_sorted_array.c
@@ -0,0 +1,32 @@
+int find_rotation_start(int *nums, int nums_size) {
+    int start = 0, end = nums_size - 2;
+    if (nums[end] > nums[end + 1]) {
+        return end + 1;
+    }
+    while (start <= end) {
+        while (start < end && nums[start] == nums[start + 1]) {
+            start += 1;
+        }
+        while (start < end && nums[end] == nums[end - 1]) {
+            end -= 1;
+        }
+        int middle = start + (end - start) / 2;
+        if (nums[middle] > nums[middle + 1]) {
+            return middle + 1;
+        } else {
+            if (nums[middle] < nums[start]) {
+                end = middle - 1;
+            } else if (nums[middle] >= nums[start]) {
+                start = middle + 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int findMin(int* nums, int numsSize) {
+    if (numsSize == 1) {
+        return nums[0];
+    }
+    return nums[find_rotation_start(nums, numsSize)];
+}
