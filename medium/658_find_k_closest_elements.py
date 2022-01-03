79fcbfeb13227064f218f7bc2d0be7bd49ef5176
Canchen Li <canchen.lee@gmail.com>
2022-01-03 14:33:54 -0600

    lc 658

--- medium/658_find_k_closest_elements.py
+++ medium/658_find_k_closest_elements.py
@@ -1,30 +1,20 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        start, end = 0, len(arr) - 1
-        while start <= end:
-            middle = (start + end) // 2
-            if arr[middle] == x:
-                start = middle + 1
-                end = middle
-                break
-            elif arr[middle] > x:
-                end = middle - 1
-            elif arr[middle] < x:
-                start = middle + 1
-        start_result, end_result = [], []
-        print(start, end)
-        while len(start_result) + len(end_result) < k:
-            if 0 <= start < len(arr) and 0 <= end < len(arr):
-                if abs(arr[start] - x) < abs(arr[end] - x):
-                    start_result.append(arr[start])
-                    start += 1
+        left_index = bisect.bisect_left(arr, x) - 1
+        right_index = left_index + 1
+        left_result, right_result = [], []
+        while len(left_result) + len(right_result) < k:
+            if 0 <= left_index < len(arr) and 0 <= right_index < len(arr):
+                if abs(arr[left_index] - x) <= abs(arr[right_index] - x):
+                    left_result.append(arr[left_index])
+                    left_index -= 1
                 else:
-                    end_result.append(arr[end])
-                    end -= 1
-            elif 0 <= start < len(arr):
-                start_result.append(arr[start])
-                start += 1
-            elif 0 <= end < len(arr):
-                end_result.append(arr[end])
-                end -= 1
-        return [*reversed(end_result)] + start_result
+                    right_result.append(arr[right_index])
+                    right_index += 1
+            elif 0 <= left_index < len(arr):
+                left_result.append(arr[left_index])
+                left_index -= 1
+            elif 0 <= right_index < len(arr):
+                right_result.append(arr[right_index])
+                right_index += 1
+        return [*reversed(left_result)] + right_result
