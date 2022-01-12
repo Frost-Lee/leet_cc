e7ad24176e8912743ffe12a8e2519c7b4933aa45
Canchen Li <canchen.lee@gmail.com>
2022-01-12 15:07:54 -0500

    lc 4

--- hard/4_median_of_two_sorted_arrays.c
+++ hard/4_median_of_two_sorted_arrays.c
@@ -1,25 +1,41 @@
-double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size){
-    int isOddLen = (nums1Size + nums2Size) % 2;
-    double median = 0;
-    int medianStartIndex = (nums1Size + nums2Size + isOddLen) / 2;
-    int n1Index = 0, n2Index = 0;
-    for (int i = 0; i < medianStartIndex; i ++) {
-        if (n1Index < nums1Size && (n2Index >= nums2Size || nums1[n1Index] < nums2[n2Index])) {
-            median = nums1[n1Index];
-            n1Index += 1;
-        } else {
-            median = nums2[n2Index];
-            n2Index += 1;
-        }
-    }
-    if (isOddLen == 1) {
-        return (double)median;
+int compared_value(int *nums, int numsSize, int index) {
+    if (index < 0) {
+        return INT_MIN;
+    } else if (index >= numsSize) {
+        return INT_MAX;
     } else {
-        if (n1Index < nums1Size && (n2Index >= nums2Size || nums1[n1Index] < nums2[n2Index])) {
-            median += nums1[n1Index];
-        } else {
-            median += nums2[n2Index];
-        }
-        return median / 2.0;
+        return nums[index];
     }
+}
+
+double findMedianSortedArrays(int* nums1, int nums1Size, int* nums2, int nums2Size) {
+    if (nums1Size < nums2Size) {
+        return findMedianSortedArrays(nums2, nums2Size, nums1, nums1Size);
+    }
+    int start = -1, end = nums1Size - 1;
+    int left_part_size = (nums1Size + nums2Size + 1) / 2;
+    while (start <= end) {
+        int middle = start + (end - start) / 2;
+        int nums2_partition = left_part_size - middle - 2;
+        int nums1_left = compared_value(nums1, nums1Size, middle);
+        int nums1_right = compared_value(nums1, nums1Size, middle + 1);
+        int nums2_left = compared_value(nums2, nums2Size, nums2_partition);
+        int nums2_right = compared_value(nums2, nums2Size, nums2_partition + 1);
+        if (nums1_left > nums2_right) {
+            end = middle - 1;
+        } else if (nums2_left > nums1_right) {
+            start = middle + 1;
+        } else {
+            double result = 0.0;
+            if ((nums1Size + nums2Size) & 1) {
+                result = nums1_left > nums2_left ? nums1_left : nums2_left;
+            } else {
+                int left = nums1_left > nums2_left ? nums1_left : nums2_left;
+                int right = nums1_right < nums2_right ? nums1_right : nums2_right;
+                result = (left + right) / 2.0;
+            }
+            return result;
+        }
+    }
+    return 0.0;
 }
