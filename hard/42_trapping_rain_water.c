378e30a5696e9eaeaa7858fd2ec87f3838e1c6d9
Canchen Li <canchen.lee@gmail.com>
2022-06-25 10:54:47 -0700

    lc 42

--- hard/42_trapping_rain_water.c
+++ hard/42_trapping_rain_water.c
@@ -1,39 +1,23 @@
-#define MIN(a,b) ((a) < (b) ? (a) : (b))
-
-int nearest_higher_index(const int* height, int height_size, int start_index, int reference_index) {
-    int max_index = start_index;
-    for (int i = start_index + 1; i < height_size; i ++) {
-        if (height[i] > height[max_index]) {
-            if (height[max_index] > height[reference_index]) {
-                break;
-            }
-            max_index = i;
-        }
-    }
-    return max_index != start_index ? max_index : -1;
-}
-
 int trap(int* height, int heightSize) {
-    int left_highest_index = 0;
-    int right_highest_index = 0;
-    int water_sum = 0;
-    for (int i = 0; i < heightSize; i ++) {
-        if (i >= right_highest_index) {
-            if (height[i] > height[left_highest_index]) {
-                left_highest_index = i;
+    int left_index = 0, right_index = heightSize - 1;
+    int left_max = height[0], right_max = height[heightSize - 1];
+    int area = 0;
+    while (left_index <= right_index) {
+        if (height[left_index] < height[right_index]) {
+            if (height[left_index] > left_max) {
+                left_max = height[left_index];
+            } else {
+                area += left_max - height[left_index];
             }
-            right_highest_index = nearest_higher_index(height, heightSize, i, left_highest_index);
-            if (right_highest_index == -1) {
-                right_highest_index = i + 1;
-                if (right_highest_index >= heightSize) {
-                    return water_sum;
-                }
+            left_index += 1;
+        } else {
+            if (height[right_index] > right_max) {
+                right_max = height[right_index];
+            } else {
+                area += right_max - height[right_index];
             }
-        }
-        int surface_height = MIN(height[right_highest_index], height[left_highest_index]);
-        if (surface_height - height[i] > 0) {
-            water_sum += surface_height - height[i];
+            right_index -= 1;
         }
     }
-    return water_sum;
+    return area;
 }
