457c2e457d0bd923462d5246d5f3be86e04788a2
Canchen Li <canchen.lee@gmail.com>
2022-07-02 16:53:54 -0700

    lc 198

--- medium/198_house_robber.c
+++ medium/198_house_robber.c
@@ -1,11 +1,12 @@
 int rob(int* nums, int numsSize) {
-    int *dp = (int *)malloc(sizeof(int) * numsSize);
+    int current = 0, previous = 0, next_previous = 0;
     for (size_t i = 0; i < numsSize; i ++) {
-        int skip_previous = i >= 2 ? dp[i - 2] : 0;
-        int previous = i >= 1 ? dp[i - 1] : 0;
-        dp[i] = skip_previous + nums[i] > previous ? skip_previous + nums[i] : previous;
+        int temp_next_previous = next_previous, temp_previous = previous;
+        next_previous = previous;
+        previous = current;
+        int include_i_value = nums[i] + next_previous;
+        int exclude_i_value = previous;
+        current = include_i_value > exclude_i_value ? include_i_value : exclude_i_value;
     }
-    int result = dp[numsSize - 1];
-    free(dp);
-    return result;
+    return current;
 }
