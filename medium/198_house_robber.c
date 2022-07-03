baf01ab0c7e4556f21da99188625142d1f9ec732
Canchen Li <canchen.lee@gmail.com>
2022-07-02 17:12:35 -0700

    lc 198

--- medium/198_house_robber.c
+++ medium/198_house_robber.c
@@ -1,7 +1,6 @@
 int rob(int* nums, int numsSize) {
     int current = 0, previous = 0, next_previous = 0;
     for (size_t i = 0; i < numsSize; i ++) {
-        int temp_next_previous = next_previous, temp_previous = previous;
         next_previous = previous;
         previous = current;
         int include_i_value = nums[i] + next_previous;
