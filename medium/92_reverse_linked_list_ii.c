6d1f6c209dd559569147e2c9270b871fd1c51d63
Canchen Li <canchen.lee@gmail.com>
2022-07-09 17:14:39 -0700

    lc 92

--- medium/92_reverse_linked_list_ii.c
+++ medium/92_reverse_linked_list_ii.c
@@ -1,28 +1,24 @@
 struct ListNode* reverseBetween(struct ListNode* head, int left, int right) {
-    size_t index = 1;
-    struct ListNode *old_head = head;
-    struct ListNode *prev = NULL, *next = NULL;
-    struct ListNode *start = NULL, *start_prev = NULL;
-    while (index <= right) {
-        next = head->next;
-        if (index == left - 1) {
-            start_prev = head;
-        } else if (index == left) {
-            start = head;
-        } else if (index > left) {
-            head->next = prev;
-        }
-        prev = head;
-        head = next;
-        index += 1;
+    struct ListNode *previous_reverse_head = NULL, *reverse_head = head;
+    int current_index = 1;
+    while (current_index < left) {
+        previous_reverse_head = reverse_head;
+        reverse_head = reverse_head->next;
+        current_index += 1;
     }
-    if (start_prev != NULL) {
-        start_prev->next = prev;
+    struct ListNode *previous = NULL, *current = reverse_head;
+    while (current_index <= right) {
+        struct ListNode *temp_next = current->next;
+        current->next = previous;
+        previous = current;
+        current = temp_next;
+        current_index += 1;
     }
-    start->next = next;
-    if (left == 1) {
-        return prev;
+    reverse_head->next = current;
+    if (previous_reverse_head != NULL) {
+        previous_reverse_head->next = previous;
+        return head;
     } else {
-        return old_head;
+        return previous;
     }
 }
