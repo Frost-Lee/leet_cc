b0401738e7b804a9cfb98557683e328cbaf0deee
Canchen Li <canchen.lee@gmail.com>
2022-07-25 23:10:44 -0700

    lc 1650

--- medium/1650_lowest_common_ancestor_of_a_binary_tree_iii.py
+++ medium/1650_lowest_common_ancestor_of_a_binary_tree_iii.py
@@ -1,20 +1,21 @@
 class Solution:
-    def nodeDepth(self, node):
+    def node_depth(self, node):
         depth = 0
-        while node is not None:
+        while node.parent is not None:
             node = node.parent
             depth += 1
         return depth
 
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        p_depth, q_depth = self.nodeDepth(p), self.nodeDepth(q)
-        while p_depth > q_depth:
-            p = p.parent
-            p_depth -= 1
-        while q_depth > p_depth:
+        p_depth, q_depth = self.node_depth(p), self.node_depth(q)
+        if p_depth > q_depth:
+            p, q = q, p
+            p_depth, q_depth = q_depth, p_depth
+        depth_difference = q_depth - p_depth
+        while depth_difference > 0:
             q = q.parent
-            q_depth -= 1
-        while id(p) != id(q):
+            depth_difference -= 1
+        while p.val != q.val:
             p = p.parent
             q = q.parent
         return p
