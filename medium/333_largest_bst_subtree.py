05f79b40f07425a8969bc6929670614eb10fe483
Canchen Li <canchen.lee@gmail.com>
2022-08-06 14:41:11 -0700

    lc 333

--- medium/333_largest_bst_subtree.py
+++ medium/333_largest_bst_subtree.py
@@ -1,19 +1,20 @@
-class Solution:
-    def dfs(self, root):
-        if root is None:
-            return (float('inf'), float('-inf'), 0, 0)    # min, max, largest, size
-        left, right = self.dfs(root.left), self.dfs(root.right)
-        new_min = min(root.val, left[0], right[0])
-        new_max = max(root.val, left[1], right[1])
-        new_largest = max(left[2], right[2])
-        new_size = left[3] + right[3] + 1
-        if left[1] < root.val and right[0] > root.val:
-            new_largest = new_size
-        else:
-            new_min = float('-inf')
-            new_max = float('inf')
-            new_size = float('-inf')
-        return (new_min, new_max, new_largest, new_size)
+import math
 
+class Solution:
     def largestBSTSubtree(self, root: Optional[TreeNode]) -> int:
-        return self.dfs(root)[2]
+        max_bst_size = 0
+        def dfs(node):
+            nonlocal max_bst_size
+            if node is None:
+                return (math.inf, -math.inf, 0, True)
+            left_min, left_max, left_size, left_bst = dfs(node.left)
+            right_min, right_max, right_size, right_bst = dfs(node.right)
+            is_bst = False
+            if left_max < node.val < right_min and left_bst and right_bst:
+                is_bst = True
+            tree_size = left_size + right_size + 1
+            if is_bst:
+                max_bst_size = max(max_bst_size, tree_size)
+            return (min(left_min, right_min, node.val), max(left_max, right_max, node.val), tree_size, is_bst)
+        dfs(root)
+        return max_bst_size
